380ca7854006b2681114ea120575c2309ca195ae
kaechesbord <damirlihtneri6@gmail.com>
2022-11-23 17:00:52 +0100

    CSS changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import "../App.css";
-import "bootstrap/dist/css/bootstrap.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye} from '@fortawesome/free-solid-svg-icons'
-import { faEyeSlash} from '@fortawesome/free-solid-svg-icons'
-
-
+import "bootstrap/dist/css/bootstrap.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const Form = () => {
-    const [showPassword, setShowPassword] = useState(false)
-    const showIcon = <FontAwesomeIcon icon={faEye} />
-    const hideIcon = <FontAwesomeIcon icon={faEyeSlash} />
+  const [showPassword, setShowPassword] = useState(false);
+  const showIcon = <FontAwesomeIcon icon={faEye} />;
+  const hideIcon = <FontAwesomeIcon icon={faEyeSlash} />;
   return (
     <div className="container">
       <div className="form">
@@ -32,15 +30,29 @@ const Form = () => {
           </div>
           <div className="password-cont">
             <div className="password">
-            <input type={showPassword ? "text" : "password"} className="input" />
-            <button className="eye" onClick={()=> setShowPassword(!showPassword)}>{showPassword ? showIcon : hideIcon }</button>
+              <input
+                type={showPassword ? "text" : "password"}
+                className="input"
+              />
+              <button
+                className="eye"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? showIcon : hideIcon}
+              </button>
             </div>
             <p>Password</p>
           </div>
         </div>
-        <button type="button" className="btn btn-success w-25 my-3 ">Submit</button>
+        <div className="btncont">
+          <button type="button" className="submit">
+            Submit
+          </button>
+        </div>
         <div className="alr">
-        <a href=" #" class="link-primary">Already have an account?</a>
+          <a href=" #" class="link-primary">
+            Already have an account?
+          </a>
         </div>
       </div>
     </div>
